3941f7e98e422c4d1b10071361106cc615c9c7a2
vpmammana <vpmammana@gmail.com>
2021-10-08 18:45:37 -0300

    Colocacao do Objeto de Controle, rotacao, deslizamento, inercia - falta fazer movimento absoluto

--- html/objetos.js
+++ html/objetos.js
@@ -24,59 +24,138 @@ function doOverlap( l1x, l1y,  r1x, r1y,  l2x, l2y,  r2x, r2y) {
     }
 
 
+export class controle_geral {
 
+constructor (){
+
+	this.selecionado = null;
+	this.atrito      = 1; // coeficiente de atrito para todos os objetos deslizantes
+	this.objetos_em_cena = [];
+	this.delta_t_animacao=10;
+	this.delta_t_simulacao= 0.01;
+	let that = this;
+	this.animacao = setInterval(function () { that.animar();}, that.delta_t_animacao);
+}
+
+animar() {
+
+let i;
+
+	for (i=0; i < this.objetos_em_cena.length; i++){
+		
+		let objeto = this.objetos_em_cena[i];
+			objeto.guarda_vx = objeto.guarda_vx + objeto.guarda_ax * this.delta_t_simulacao;
+			objeto.guarda_ax = - objeto.guarda_vx * objeto.atrito;
+			objeto.guarda_vy = objeto.guarda_vy + objeto.guarda_ay * this.delta_t_simulacao;
+			objeto.guarda_ay = - objeto.guarda_vy * objeto.atrito;
+			objeto.posicao_percentual_x = objeto.posicao_percentual_x + objeto.guarda_vx * this.delta_t_simulacao;
+			objeto.posicao_percentual_y = objeto.posicao_percentual_y + objeto.guarda_vy * this.delta_t_simulacao;
+	
+	
+	} // fim for
+
+	
+} // fim metodo animacao
+
+} // fim class controle_geral
 
 export class movel {
    estado="indefinido";
 
-constructor (id, arquivo){
+constructor (id, arquivo, nome_fantasia, controle){
 	this.automatiza=null;
-	this.velho_rotacao = 0;
 	this.automatiza_giros=null;
+	
+	this.guarda_rotacao = 0;
+	this.id = id;
 	this.delta_t=10; // ms
 
+	this.atrito = controle.atrito;
+
+	this.massa=1;
+
+	this.guarda_vx=0;
+	this.guarda_vy=0;
+	this.guarda_ax=0;
+	this.guarda_ay=0;
+	this.guarda_Fx=0;
+	this.guarda_Fy=0;
+		
+	
 	this.lista_de_detecao=[];
+	this.lista_de_fantasias=[];
+	this.velho_fantasia=0;
+	this.fantasia=0; // zero indica que não tem fantasia
 
 	this.conta_passos=0;
 	
 	this.conta_giros=0;
 
-	this.fantasia=document.createElement("img");
-	this.fantasia.style.pai = this;
 	this.largura_container=document.body.clientWidth;
 	this.altura_container=document.body.clientHeight;
+	this.guarda_largura_percentual=100;
+	this.guarda_altura_percentual=100;
+	this.guarda_posicao_percentual_x = 50;
+	this.guarda_posicao_percentual_y = 50;
+	this.acrescenta_fantasia(arquivo, nome_fantasia);
+	controle.objetos_em_cena.push(this);
 
-	let that=this; // isto é necessário porque no ambito do tratamento de eventos onerror e onload do IMG, this eh o img
-	document.body.appendChild(this.fantasia);
-	this.fantasia.id=id;
-	this.fantasia.style.position = "absolute";
-	this.fantasia.onerror=function (){that.nao_achou_imagem()}
-	this.fantasia.onload=function (){that.achou_imagem();}
+}
 
-	this.fantasia.src=arquivo;
+set Fx(valor) {
+	this.guarda_Fx = valor;
+	this.impulso(this.guarda_Fx, 0);
+	this.guarda_Fx = 0;	
 
-	this.fantasia.alt="erro: "+arquivo+" não encontrado";
+}
 
-	this.largura=this.fantasia.width;
-	this.altura =this.fantasia.height;
-	this.velho_largura_percentual=100;
-	this.velho_altura_percentual=100;
+set Fy(valor) {
+	this.guarda_Fy = valor;
+	this.impulso(0, this.guarda_Fy);
+	this.guarda_Fy = 0;	
 
+}
 
-	this.posiciona_percentual(50,50);
-	this.velho_posicao_percentual_x = 50;
-	this.velho_posicao_percentual_y = 50;
+get Fx(){
+	return this.guarda_Fx;
+}
 
-	this.fantasia.style.visibility="visible";
+get Fy(){
+	return this.guarda_Fy;
+}
+
+impulso (fx,fy) {
+	this.guarda_ax = fx / this.massa;	
+	this.guarda_ay = fy / this.massa;	
+
+}
+
+atualiza_fantasia() {
+
+	if (this.velho_fantasia != this.fantasia) {
+		if ( this.velho_fantasia >0 ) { this.lista_de_fantasias[this.velho_fantasia - 1].style.visibility = "hidden";}
+		this.lista_de_fantasias[this.fantasia - 1].style.visibility = "visible";
+		this.velho_fantasia = this.fantasia;
+	}
+
+}
+
+set fantasia_atual(valor){
+	this.fantasia=valor;
+	this.tamanho_percentual(this.guarda_largura_percentual, this.guarda_altura_percentual);
+}
+
+get fantasia_atual(){
+	return this.fantasia;
 }
 
 get rotacao(){
-	return this.velho_rotacao;
+	return this.guarda_rotacao;
 }
 
 set rotacao(graus){
 	this.rotaciona(graus);
-	this.velho_rotacao = graus;
+	this.guarda_rotacao = graus;
 }
 
 get estado_prop() {
@@ -88,35 +167,35 @@ set estado_prop(valor) {
 }
 
 get largura_percentual (){
-	return this.velho_largura_percentual;
+	return this.guarda_largura_percentual;
 }
 
 get altura_percentual (){
-	return this.velho_altura_percentual;
+	return this.guarda_altura_percentual;
 }
 
 set largura_percentual (value){
-	this.tamanho_percentual(value,this.velho_altura_percentual);
+	this.tamanho_percentual(value,this.guarda_altura_percentual);
 }
 
 set altura_percentual (value){
-	this.tamanho_percentual(this.velho_largura_percentual,value);
+	this.tamanho_percentual(this.guarda_largura_percentual,value);
 }
 
 set posicao_percentual_x (value){
-	this.posiciona_percentual(value,this.velho_posicao_percentual_y);
+	this.posiciona_percentual(value,this.guarda_posicao_percentual_y);
 }
 
 set posicao_percentual_y (value){
-	this.posiciona_percentual(this.velho_posicao_percentual_x,value);
+	this.posiciona_percentual(this.guarda_posicao_percentual_x,value);
 }
 
 get posicao_percentual_x (){
-	return this.velho_posicao_percentual_x;
+	return this.guarda_posicao_percentual_x;
 }
 
 get posicao_percentual_y (){
-	return this.velho_posicao_percentual_y;
+	return this.guarda_posicao_percentual_y;
 }
 
 achou_imagem(){
@@ -129,39 +208,54 @@ nao_achou_imagem(){
 }
 
 rotaciona (graus){
-	this.fantasia.style.transform="rotate(" + graus + "deg)";
+	this.lista_de_fantasias[this.fantasia - 1].style.transform="rotate(" + graus + "deg)";
+	this.atualiza_fantasia();
 }
 
 posiciona_percentual(x,y){
-	this.fantasia.style.top=((this.altura_container) - (this.altura_container) * y/100) - this.fantasia.height/2;
-	this.fantasia.style.left=this.largura_container * x/100 - this.fantasia.width/2;
-	this.velho_posicao_percentual_x=x;
-	this.velho_posicao_percentual_y=y;
+	this.lista_de_fantasias[this.fantasia - 1].style.top=((this.altura_container) - (this.altura_container) * y/100) - this.lista_de_fantasias[this.fantasia - 1].height/2;
+	this.lista_de_fantasias[this.fantasia - 1].style.left=this.largura_container * x/100 - this.lista_de_fantasias[this.fantasia - 1].width/2;
+	this.guarda_posicao_percentual_x=x;
+	this.guarda_posicao_percentual_y=y;
+	this.atualiza_fantasia();
 
 }
 
+
+
 tamanho_percentual(x,y){
-	this.velho_largura_percentual=x;
-	this.velho_altura_percentual=y;
-	this.fantasia.height = this.altura * y/100;
-	this.fantasia.width  = this.largura * x/100;
+	this.guarda_largura_percentual=x;
+	this.guarda_altura_percentual=y;
+	console.log(this.lista_de_fantasias[0]);
+	console.log(this.fantasia);
+	this.lista_de_fantasias[this.fantasia - 1].height = this.altura * y/100;
+	this.lista_de_fantasias[this.fantasia - 1].width  = this.largura * x/100;
 	this.posiciona_percentual(this.posicao_percentual_x, this.posicao_percentual_y);
+	this.atualiza_fantasia();
 }
 
 gira(delta_graus, giros){
+	if ( this.automatiza_giros != null ) { clearInterval(this.automatiza_giros); this.automatiza_giros=null;}
 	this.conta_giros = 0;
-	let that=this
+	let that=this;
+	
 	this.automatiza_giros = setInterval(function ()
 			{ 
-				if ( that.conta_giros < giros ) {
-					that.rotacao = that.rotacao + delta_graus;
+				console.log(that.rotacao);
+				if ( that.conta_giros < parseFloat(giros) ) {
+					that.rotacao = that.rotacao + parseFloat(delta_graus);
 					that.conta_giros++;
 				}
 				else {
 					clearInterval(that.automatiza_giros);
+					that.automatiza_giros = null;
 				}
 			},this.delta_t);	
 	
+}
+
+para_giro(){
+ 	if (this.automatiza_giros != null ) {clearInterval(this.automatiza_giros); this.automatiza_giros=null;}
 }
 
 verifica_se_bateu (){
@@ -169,15 +263,15 @@ verifica_se_bateu (){
 	for (i=0; i < this.lista_de_detecao.length; i++) {
 		let objeto=this.lista_de_detecao[i];
 		//console.log(objeto);
-		let l1x = objeto.fantasia.getBoundingClientRect().left;
-		let l1y = objeto.fantasia.getBoundingClientRect().top;
-		let r1x = objeto.fantasia.getBoundingClientRect().right;
-		let r1y = objeto.fantasia.getBoundingClientRect().bottom;
+		let l1x = objeto.lista_de_fantasias[objeto.fantasia -1].getBoundingClientRect().left;
+		let l1y = objeto.lista_de_fantasias[objeto.fantasia -1].getBoundingClientRect().top;
+		let r1x = objeto.lista_de_fantasias[objeto.fantasia -1].getBoundingClientRect().right;
+		let r1y = objeto.lista_de_fantasias[objeto.fantasia -1].getBoundingClientRect().bottom;
 		
-		let l2x = this.fantasia.getBoundingClientRect().left;
-		let l2y = this.fantasia.getBoundingClientRect().top;
-		let r2x = this.fantasia.getBoundingClientRect().right;
-		let r2y = this.fantasia.getBoundingClientRect().bottom;
+		let l2x = this.lista_de_fantasias[this.fantasia - 1].getBoundingClientRect().left;
+		let l2y = this.lista_de_fantasias[this.fantasia - 1].getBoundingClientRect().top;
+		let r2x = this.lista_de_fantasias[this.fantasia - 1].getBoundingClientRect().right;
+		let r2y = this.lista_de_fantasias[this.fantasia - 1].getBoundingClientRect().bottom;
 		//console.log( doOverlap( l1x, l1y,  r1x, r1y,  l2x, l2y,  r2x, r2y) );
 
 		if ( doOverlap( l1x, l1y,  r1x, r1y,  l2x, l2y,  r2x, r2y) ) { console.log("bateu");}
@@ -192,17 +286,59 @@ desliza(delta_x, delta_y, passos){
 	let that = this;
 	this.automatiza = setInterval(function ()
 			{ 
-				if ( that.conta_passos < passos ) {
+				if ( that.conta_passos < parseFloat(passos) ) {
 					that.verifica_se_bateu();
-					that.posicao_percentual_x = that.posicao_percentual_x + delta_x;
-					that.posicao_percentual_y = that.posicao_percentual_y + delta_y;
+					that.posicao_percentual_x = that.posicao_percentual_x + parseFloat(delta_x);
+					that.posicao_percentual_y = that.posicao_percentual_y + parseFloat(delta_y);
 					that.conta_passos++;
 				}
 				else {
 					clearInterval(that.automatiza);
+					that.automatiza = null;
 				}
 			},this.delta_t);	
 		
+}
+
+para_desliza() {
+	if (this.automatiza != null) {clearInterval(this.automatiza); this.automatiza=null;}	
+}
+
+
+proxima_fantasia(){
+	this.fantasia++;
+	this.atualiza_fantasia();	
+}
+
+acrescenta_fantasia(arquivo, nome){
+
+	let fantasy=document.createElement("img");
+	fantasy.style.pai = this;
+	
+	fantasy.style.visibility="hidden";
+	document.body.appendChild(fantasy);
+	fantasy.id=this.id + "_" + nome;
+	fantasy.style.position = "absolute";
+	let that = this;
+	fantasy.onerror=function (){that.nao_achou_imagem()}
+	fantasy.onload=function (){that.achou_imagem();}
+
+	fantasy.src=arquivo;
+
+	fantasy.addEventListener("click", ()=> {
+		controle.selecionado = that;
+          });
+
+
+	fantasy.alt="erro: "+arquivo+" não encontrado";
+	this.lista_de_fantasias.push(fantasy);
+	this.velho_fantasia = this.fantasia;
+	this.fantasia = this.lista_de_fantasias.length;
+
+	this.largura = fantasy.width;
+	this.altura  = fantasy.height;
+	this.tamanho_percentual(this.guarda_largura_percentual, this.guarda_largura_percentual);
+
 }
 
 
